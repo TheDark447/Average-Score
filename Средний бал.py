b49bdb6bdef6055788d19e914e480cf6adc230cf
Dart Vader <adigam80@gmail.com>
2024-12-10 19:28:34 +0500

    Add files via upload

---
+++ Средний бал.py
@@ -0,0 +1,4 @@
+grades = [[(5+3+3+5+4)/5], [(2+2+2+3)/4], [(4+5+5+2)/4], [(4+4+3)/3], [(5+5+5+4+5)/5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = {'Johnny':(grades[3]),'Bilbo':(grades[1]),'Steve':(grades[2]),'Khendrik':(grades[4]),'Aaron':(grades[0])}
+print(students)
